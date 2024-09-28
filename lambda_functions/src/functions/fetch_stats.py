c391d98090e60438ffa74b4f4c45d8bfa006f297
tim506877 <tim506877@gmail.com>
2024-09-28 13:05:57 -0400

    Add FB to ml model

--- lambda_functions/src/functions/fetch_stats.py
+++ lambda_functions/src/functions/fetch_stats.py
@@ -10,6 +10,7 @@ sys.path.append(parent_dir)
 from utils.today_statcast_hh_data_fetcher import FangraphsStatcastScraper
 from utils.today_dashboard_ISO_fetcher import FangraphsDashboardScraper
 from utils.today_pitcher_starter_fetcher import PitcherScraperAPI
+from utils.today_stats_fb_fetcher import FangraphsStatsScraper
 from utils.s3_uploader import upload_to_s3
 from utils.s3_uploader import delete_from_s3
 
@@ -22,6 +23,7 @@ def lambda_handler(event, context):
     delete_from_s3(BUCKET_NAME, 'today_iso_data.csv');
     delete_from_s3(BUCKET_NAME, 'today_hh_data.csv');
     delete_from_s3(BUCKET_NAME, 'today_pitcher_data.csv');
+    delete_from_s3(BUCKET_NAME, 'today_fb_data.csv');
 
     # Get today's date and get today's hh and iso data
     # Define the EST timezone
@@ -33,18 +35,21 @@ def lambda_handler(event, context):
     # Save csv to /tmp as well
     FangraphsDashboardScraper.get_batter_iso_for_date(start_date, file_path="/tmp/today_iso_data.csv")
     FangraphsStatcastScraper.get_batter_hh_for_date(start_date, file_path="/tmp/today_hh_data.csv")
+    FangraphsStatsScraper.get_batter_fb_for_date(start_date, file_path="/tmp/today_fb_data.csv")
     PitcherScraperAPI.scrape_data(str(today), file_path="/tmp/today_pitcher_data.csv")
 
     # Write to /tmp directory for Lambda environment 
     today_iso_data_path = '/tmp/today_iso_data.csv'
     today_hh_data_path = '/tmp/today_hh_data.csv'
+    today_fb_data_path = '/tmp/today_fb_data.csv'
     today_pitcher_data_path = '/tmp/today_pitcher_data.csv'
+
 
     # Upload to S3
     upload_to_s3(today_iso_data_path, BUCKET_NAME, 'today_iso_data.csv')
     upload_to_s3(today_hh_data_path, BUCKET_NAME, 'today_hh_data.csv')
     upload_to_s3(today_pitcher_data_path, BUCKET_NAME, 'today_pitcher_data.csv')
-
+    upload_to_s3(today_fb_data_path, BUCKET_NAME, 'today_fb_data.csv')
     return {
         'statusCode': 200,
         'body': json.dumps('Data fetched and saved successfully')
